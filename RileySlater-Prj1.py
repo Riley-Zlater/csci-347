8b7d9fdfe7b0645fb4d9ed309067607e11284a92
Riley-Zlater <71993701+Riley-Zlater@users.noreply.github.com>
2021-01-22 14:56:48 -0700

    Update RileySlater-Prj1.py

--- RileySlater-Prj1.py
+++ RileySlater-Prj1.py
@@ -3,19 +3,19 @@ import numpy as np
 
 def find_mean(2D_array):
 
-    file1 = 0
-    file2 = 0
+    col1 = 0
+    col2 = 0
     counter = 0
     
     for row in 2D_array:
         counter +=1
-        file1 += row[0]
-        file2 += row[1]
+        col1 += row[0]
+        col2 += row[1]
 
-    file1 = file1 / counter
-    file2 = file2 / counter
+    col1 = col1 / counter
+    col2 = col2 / counter
 
-    return np.array([file1, file2])
+    return np.array([col1, col2])
 
 def find_covariance(array1, array2):
 
