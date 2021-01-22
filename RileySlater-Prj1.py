f7cf5cbb9b55d3496556a12f483541ac970a560a
Riley-Zlater <71993701+Riley-Zlater@users.noreply.github.com>
2021-01-22 14:54:51 -0700

    Update RileySlater-Prj1.py

--- RileySlater-Prj1.py
+++ RileySlater-Prj1.py
@@ -22,3 +22,14 @@ def find_covariance(array1, array2):
     D = np.stack((array1, array2), axis=1)
 
     m_array = find_mean(D)
+
+    num = 0
+    den = 0
+    
+    for row in D:
+        den += 1
+        num += (row[0] - m_array[0])*(row[1] - m_array[1])
+
+    cov = num / den
+
+    return cov
