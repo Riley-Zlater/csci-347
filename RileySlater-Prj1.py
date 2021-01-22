8c494760080630ffc215d2e99c896f10ea953ee4
Riley-Zlater <71993701+Riley-Zlater@users.noreply.github.com>
2021-01-22 15:11:20 -0700

    Update RileySlater-Prj1.py

--- RileySlater-Prj1.py
+++ RileySlater-Prj1.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import math
 
 def find_mean(2D_array):
 
@@ -20,7 +20,6 @@ def find_mean(2D_array):
 def find_covariance(array1, array2):
 
     D = np.stack((array1, array2), axis=1)
-
     m_array = find_mean(D)
 
     num = 0
@@ -33,3 +32,16 @@ def find_covariance(array1, array2):
     cov = num / den
 
     return cov
+
+def find_correlation(array1, array2):
+
+    D = np.stack((array1, array2) , axis=1)
+    m_array = find_mean(D)
+
+    std1 = 0
+    std2 = 0
+
+    for row in D:
+        std1 += (row[0] - m_array[0]) ** 2
+    
+
