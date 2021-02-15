57a4adbdf61fff155e8bd0ce099391b7f991e4ef
Riley Slater <rileymail6@gmail.com>
2021-02-15 11:41:38 -0700

    finished function for degree of a node

--- Project 2/graphLib.py
+++ Project 2/graphLib.py
@@ -3,39 +3,49 @@ import networkx as nx
 # Written by Alexander Alvarez and Riley Slater
 # Write functions to perform graph analysis
 
+
+# edge list used for testing the functions
+#+-------------+
 testEdgeList = 'C:\\Users\\riley\\Desktop\\CSCI 347\\bn-cat-mixed-species_brain_1.edges'
+#+-------------+
+
 
 def numVert(edgeList):
     G = nx.read_edgelist(edgeList)
     return len(G)
 
-#TODO: write a function the returns the degree of a given vertex
+
 def degVert(edgeList, vertex):
-    G = read_edgelist(edgeList)
-    pass
+    G = nx.read_edgelist(edgeList)
+    
+    if str(vertex) not in G:
+        print("Vertex", vertex, "is not in the graph.")
+    else:
+        return len([n for n in G[str(vertex)]])
 
 
 #TODO: write a function that returns the clustering coefficient of a given vertex
 def clustCoeff(edgeList, vertex):
-    G = read_edgelist(edgeList)
+    G = nx.read_edgelist(edgeList)
     pass
 
 
 #TODO: write a function that returns the betweenness centrality of a given vertex
 def betweenCent(edgeList, vertex):
-    G = read_edgelist(edgeList)
+    G = nx.read_edgelist(edgeList)
     pass
 
 
 #TODO: write a function that returns the average shortest paths length of the graph
 def avgShortPathLength(edgeList):
-    G = read_edgelist(edgeList)
+    G = nx.read_edgelist(edgeList)
     pass
 
 
 #TODO: write a function that returns the adjacency matrix of the graph
 def adjMatrix(edgeList):
-    G = read_edgelist(edgeList)
+    G = nx.read_edgelist(edgeList)
     pass
 
-print(numVert(testEdgeList))
+
+print(degVert(testEdgeList, 5))
