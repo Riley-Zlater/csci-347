86e351d965fc884978d57329f1bfa1656b00313d
Riley Slater <rileymail6@gmail.com>
2021-02-15 10:50:49 -0700

    create graphLib skeleton

---
+++ Project 2/graphLib.py
@@ -0,0 +1,33 @@
+
+# Written by Alexander Alvarez and Riley Slater
+# Write functions to perform graph analysis
+
+
+#TODO: write a funtion that returns the number of vertices
+def numVert(edgeList):
+    pass
+
+#TODO: write a function the returns the degree of a given vertex
+def degVert(edgeList, vertex):
+    pass
+
+
+#TODO: write a function that returns the clustering coefficient of a given vertex
+def clustCoeff(edgeList, vertex):
+    pass
+
+
+#TODO: write a function that returns the betweenness centrality of a given vertex
+def betweenCent(edgeList, vertex):
+    pass
+
+
+#TODO: write a function that returns the average shortest paths length of the graph
+def avgShortPathLength(edgeList):
+    pass
+
+
+#TODO: write a function that returns the adjacency matrix of the graph
+def adjMatrix(edgeList):
+    pass
+
