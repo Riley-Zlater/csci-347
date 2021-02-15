443154e0203b27684b5d22eb9f4923c976ecb243
Riley Slater <rileymail6@gmail.com>
2021-02-15 15:55:47 -0700

    finished betweenness centrality function

--- Project 2/graphLib.py
+++ Project 2/graphLib.py
@@ -31,10 +31,17 @@ def clustCoeff(edgeList, vertex):
     return 2.0*triDeg / (degVert(edgeList, vertex)*(degVert(edgeList, vertex)-1))
 
 
-#TODO: write a function that returns the betweenness centrality of a given vertex
 def betweenCent(edgeList, vertex):
     G = nx.read_edgelist(edgeList)
-    pass
+    betweenness = dict.fromkeys(G, 0.0)
+    nodes = G.nodes()
+
+    for i in nodes:
+        X, Y, sig = helperSearch(G, i)
+        betweenness = helperAccumulate(betweenness, X, Y, sig, i)
+    betweenness = helperScale(betweenness, len(G))
+    
+    return betweenness[str(vertex)]
 
 
 #TODO: write a function that returns the average shortest paths length of the graph
@@ -47,3 +54,49 @@ def avgShortPathLength(edgeList):
 def adjMatrix(edgeList):
     G = nx.read_edgelist(edgeList)
     pass
+
+
+
+
+
+def helperSearch(G, node):
+    X = []
+    Y = {}
+    for n in G:
+        Y[n] = []
+    sig = dict.fromkeys(G, 0.0)
+    D = {}
+    sig[node] = 1.0
+    D[node] = 0
+    Q = [node]
+    while Q:
+        n = Q.pop(0)
+        X.append(n)
+        Dn = D[n]
+        sign = sig[n]
+        for i in G[n]:
+            if i not in D:
+                Q.append(i)
+                D[i] = Dn + 1
+            if D[i] == Dn + 1:
+                sig[i] += sign
+                Y[i].append(n)
+    return X, Y, sig
+
+def helperAccumulate(betweenness, X, Y, sig, i):
+    D = dict.fromkeys(X, 0)
+    while X:
+        j = X.pop()
+        coef = (1.0 + D[j]) / sig[j]
+        for n in Y[j]:
+            D[n] += sig[n] * coef
+        if j != i:
+            betweenness[j] += D[j]
+    return betweenness
+
+def helperScale(betweenness, graphSize):
+    s = 1.0/((graphSize-1)*(graphSize-2))
+    for n in betweenness:
+        betweenness[n] *= s
+    return betweenness
+            
