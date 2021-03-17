cdf722c0ba251f03b5182265f30b4b42c58330c6
Alexander Alvarez <ap.alvarez1100@gmail.com>
2021-03-17 13:42:41 -0600

    dbscan refactor, plotting and testing

---
+++ Project 3/activities.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import clusterLib as cl
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
+
+# plot the first 2 attributes of the data
+def drawDBSCAN_2D(labeledData):
+    plt.scatter(np.array(labeledData[0])[:, 0], np.array(labeledData[0])[:, 1], c='r', marker='.')
+    for i in range(1, len(labeledData.keys())):
+        plt.scatter(np.array(labeledData[i]['C'])[:, 0], np.array(labeledData[i]['C'])[:, 1], c='g', marker='X')
+        if len(labeledData[i]['B']) == 0:
+            continue
+        plt.scatter(np.array(labeledData[i]['B'])[:, 0], np.array(labeledData[i]['B'])[:, 1], c='b', marker='p')
+    plt.show()
+
+# import data
+datafile = open('seeds_dataset.txt', 'r')
+lines = datafile.readlines()
+data = np.array([line.split() for line in lines]).astype(float)
+print(pd.DataFrame(data))
+
+#PCA the data to two attributes and plot the dbscan
+pca = PCA(n_components=2)
+pca.fit(data)
+pcaData = pca.transform(data)
+print(pcaData)
+drawDBSCAN_2D(cl.dbscan(pcaData, .7, 9))
+
--- Project 3/clusterLib.py
+++ Project 3/clusterLib.py
@@ -1,9 +1,9 @@
 import numpy as np
 from sklearn.metrics import pairwise_distances_argmin
 
+
 # k - means written by Riley Slater
 def kmeans(data, k, epsilon=None, seed=0):
-
     # randomly select clusters
     randomNum = np.random.RandomState(seed)
     randomData = randomNum.permutation(data.shape[0])[:k]
@@ -16,7 +16,7 @@ def kmeans(data, k, epsilon=None, seed=0):
 
         # centers found from the means
         newCenter = np.array([data[label == i].mean(0)
-                               for i in range(k)])
+                              for i in range(k)])
 
         # convergence check
         # If the convergence parameter
@@ -28,41 +28,70 @@ def kmeans(data, k, epsilon=None, seed=0):
 
     return center, label
 
+
 # DBSCAN written by Alexander Alvarez
 def dbscan(data, eps, minpts):
+    # initialization
     C = 0
-
-    label = {}
+    labels = {
+        0: [] #<- noise
+        #1: {  <- cluster #1
+            #C: [] <- core points
+            #B: [] <- boundary points
+    }
+    # loop over each point
     for p in data:
-        i = data.index(p)
-        if i in label.keys():
+        i = data.tolist().index(p.tolist())
+
+        # if point is labeled, skip
+        if isLabeled(labels, p):
             continue
+
+        # find neighbors
         nbrs = findInRange(p, data, eps)
-        if len(nbrs)+1 < minpts:
-            label[i] = (0, 'N')
+
+        # if there aren't enough neighbors, label point as noise
+        if len(nbrs) + 1 < minpts:
+            labelPoint(labels, 0, p)
             continue
+
+        # create cluster and label point as core
         C += 1
-        label[i] = (C, 'C')
-        labelNbrs(C, nbrs, minpts, eps, data, label)
+        labels[C] = {
+            'C': [],
+            'B': []
+        }
+        labelPoint(labels, [C, 'C'], p)
+
+        # label neighbors with cluster
+        labelNbrs(C, nbrs, minpts, eps, data, labels)
+
+    return labels
 
 
-
-def labelNbrs(C, nbrs, minpts, eps, data, label):
+def labelNbrs(C, nbrs, minpts, eps, data, labels):
+    # loop over each neighbor
     for q in nbrs:
-        n = data.index(q)
-        if n in label.keys():
-            if label[n] == (0, 'N'):
-                label[n] = (C, 'B')
+        n = data.tolist().index(q.tolist())
+
+        # if neighbor is labeled, continue
+        if isLabeled(labels, q):
+            # if neighbor is labeled as noise, relabel
+            if isLabeledNoise(labels,  q):
+                labelPoint(labels, [C, 'B'], q)
             continue
-        label[n] = (C, 'B')
+
+        # find secondary neighbors
         nbrs2 = findInRange(q, data, eps)
-        if nbrs2 + 1 >= minpts:
-            label[n] =(C, 'C')
-            labelNbrs(C, nbrs2, minpts, eps, data, label)
 
+        # if there are enough neighbors, label point as core, and label neighbors
+        if len(nbrs2) + 1 >= minpts:
+            labelPoint(labels, [C, 'C'], q)
+            labelNbrs(C, nbrs2, minpts, eps, data, labels)
 
-
-
+        # otherwise label neighbor as boundary
+        else:
+            labelPoint(labels, [C, 'B'], q)
 
 
 def findInRange(p, data, dist):
@@ -70,9 +99,32 @@ def findInRange(p, data, dist):
     for v in data:
         if np.linalg.norm(p - v) <= dist:
             pts.append(v)
-    return pts;
+    return pts
 
-###TEST###
+def isLabeled(labels, point):
+    p = point.tolist()
+    for cluster, label in labels.items():
+        if cluster == 0:
+            if p in label:
+                return True
+        else:
+            if p in label['C']:
+                return True
+            elif p in label['B']:
+                return True
+
+def isLabeledNoise(labels, point):
+    if point.tolist() in labels[0]:
+        return True
+
+# label is 0 for Noise or [i, 'C'|'B'], where i = cluster #, 'C' = core point, 'B' = boundary point
+def labelPoint(labels, label, point):
+    if label == 0:
+        labels[0].append(point.tolist())
+    else:
+        labels[label[0]][label[1]].append(point.tolist())
+
+### TEST ###
 from sklearn.cluster import KMeans
 
 X = np.array([[1, 2], [1, 4], [1, 0],
@@ -81,7 +133,7 @@ X = np.array([[1, 2], [1, 4], [1, 0],
 skmeans = KMeans(n_clusters=2, random_state=0).fit(X)
 centers, labels = kmeans(X, 2)
 
-print("(seed = 0) sklearn alg labels:\n",skmeans.labels_)
-print("(seed = 0) our alg labels\n",labels,'\n')
-print("(seed = 0) sklearn alg cluster centers:\n",skmeans.cluster_centers_)
-print("(seed = 0) our alg cluster centers:\n",centers)
+print("(seed = 0) sklearn alg labels:\n", skmeans.labels_)
+print("(seed = 0) our alg labels\n", labels, '\n')
+print("(seed = 0) sklearn alg cluster centers:\n", skmeans.cluster_centers_)
+print("(seed = 0) our alg cluster centers:\n", centers)
