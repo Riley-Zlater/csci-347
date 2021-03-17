7f3ef72dd66c0fa149b1599d1dcd85a0a3f3e73d
riley <rileymail6@gmail.com>
2021-03-17 11:12:58 -0600

    kmeans updates

--- Project 3/clusterLib.py
+++ Project 3/clusterLib.py
@@ -1,9 +1,49 @@
 import numpy as np
+from sklearn.metrics import pairwise_distances_argmin
 
 # k - means written by Riley Slater
-def kmeans():
-    pass
+def kmeans(data, numClusters, epsilon=None, seed=0):
+
+    # randomly select clusters
+    randomNum = np.random.RandomState(seed)
+    randomData = randomNum.permutation(data.shape[0])[:numClusters]
+    center = data[randomData]
+     
+    while True:
+         
+        # labels based on closest center
+        label = pairwise_distances_argmin(data, center)
+
+        # centers found from the means
+        newCenter = np.array([data[label == i].mean(0)
+                               for i in range(numClusters)])
+
+        # convergence check
+        # If the convergence parameter
+        # is epsilon then should we be chekcing if
+        # center == epsilon?
+        if np.all(center == newCenter):
+            break
+        center = newCenter
+
+    return center, label
 
 # DBSCAN written by Alexander Alvarez
 def dbscan() :
     pass
+
+
+
+        ###TEST###
+from sklearn.cluster import KMeans
+
+X = np.array([[1, 2], [1, 4], [1, 0],
+              [10, 2], [10, 4], [10, 0]])
+
+skmeans = KMeans(n_clusters=2, random_state=0).fit(X)
+centers, labels = kmeans(X, 2)
+
+print("(seed = 0) sklearn alg labels:\n",skmeans.labels_)
+print("(seed = 0) our alg labels\n",labels,'\n')
+print("(seed = 0) sklearn alg cluster centers:\n",skmeans.cluster_centers_)
+print("(seed = 0) our alg cluster centers:\n",centers)
